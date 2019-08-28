abc16d0a1fbad5513025f6fee37c5257a18d6b26
Michael Lu <michaelwlu@live.com>
2019-08-28 10:52:34 -0600

    Split out Footer component, added Github link

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/styles';
+import { Theme, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    footer: {
+      textAlign: 'center',
+      marginTop: 'auto',
+      padding: theme.spacing(2),
+    },
+  }),
+);
+
+const Footer: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <Typography variant="body1">Made by Michael Lu</Typography>
+      <Typography variant="body1">
+        View the source code on{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/MikeLuDev/salad-generator"
+        >
+          Github
+        </a>
+      </Typography>
+    </footer>
+  );
+};
+
+export default Footer;
