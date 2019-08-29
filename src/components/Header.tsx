2bb85030f3e7fb1ccc923a817e34cf557ce5113d
Michael Lu <michaelwlu@live.com>
2019-08-29 13:46:36 -0600

    Tweaked styles, removed commented code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import {
   AppBar,
   Typography,
   Toolbar,
-  Container,
   makeStyles,
   Theme,
   createStyles,
@@ -11,51 +10,26 @@ import {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      marginBottom: theme.spacing(2),
-    },
     h1: {
       fontSize: '20px',
       fontWeight: 500,
+    },
+    toolbar: {
+      display: 'flex',
+      justifyContent: 'center',
     },
   }),
 );
 
 const Header: React.FC = () => {
   const classes = useStyles();
-  /* const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLElement>) =>
-    setAnchorEl(event.currentTarget as Element);
-  const handleClose = () => setAnchorEl(null); */
 
   return (
-    <AppBar className={classes.root} color="primary" position="static">
-      <Toolbar>
-        <Container style={{ textAlign: 'center' }} maxWidth="sm">
-          <Typography className={classes.h1} variant="h1">
-            Salad Generator
-          </Typography>
-        </Container>
-        {/*  <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleClick}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id="main-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={!!anchorEl}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Settings</MenuItem>
-          <MenuItem onClick={handleClose}>Other Place</MenuItem>
-          <MenuItem onClick={handleClose}>Somewhere Else</MenuItem>
-        </Menu> */}
+    <AppBar color="primary" position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.h1} variant="h1">
+          Salad Generator
+        </Typography>
       </Toolbar>
     </AppBar>
   );
